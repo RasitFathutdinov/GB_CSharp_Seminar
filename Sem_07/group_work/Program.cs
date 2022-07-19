a93f4ae8d576d82a5871c92f451125e638723451
Rashit <gorinnose@yandex.ru>
2022-07-20 01:19:46 +0300

    Домашнее задание к семинару № 7

---
+++ Sem_07/group_work/Program.cs
@@ -0,0 +1,197 @@
+﻿// int[] CreateRandomArray(int N, int start, int end)  // автоматическое заполнение массива сл. числами
+//  {     
+//     int[] RandomArray = new int[N];     
+//     for (int i = 0; i < N; i++)     
+//     {         
+//         RandomArray[i] = new Random().Next(start, end + 1);     
+//     }     
+//     return RandomArray; 
+// }
+
+// int[] CreateArray()   // заполнение массива пользователем
+// {     
+//     Console.WriteLine("Введите количество элементов массива");     
+//     int size = Convert.ToInt32(Console.ReadLine());     
+//     int[] RandomArray = new int[size];     
+//     for (int i = 0; i < size; i++)     
+//     {            
+//         Console.WriteLine($"Введите {i+1} элемент массива");         
+//         RandomArray[i] = Convert.ToInt32(Console.ReadLine());     
+//     }     
+//     return RandomArray;
+// }
+
+// void ShowArray(int[] array)   // вывод массива на экран
+// {     
+//     for (int i = 0; i < array.Length; i++)     
+//     {         
+//         Console.Write(array[i] + " ");     
+//     }     
+//     Console.WriteLine(); 
+//  }
+
+// Console.Clear(); 
+
+// // Демонстрация автозаполнения массива псевдослучайными числами
+// Console.WriteLine("Введите количество элементов в массиве"); 
+// int num = Convert.ToInt32(Console.ReadLine()); 
+// Console.WriteLine("Введите первое число случайно генерируемого диапазона"); 
+// int min = Convert.ToInt32(Console.ReadLine()); 
+// Console.WriteLine("Введите последнее число случайно генерируемого диапазона"); 
+// int max = Convert.ToInt32(Console.ReadLine());  
+// int[] myRandomArray = CreateRandomArray(num, min, max); 
+// ShowArray(myRandomArray); 
+// Console.WriteLine("-------"); 
+
+// // Демонстрация заполнения массива пользователем
+// int[] myArray = CreateArray(); 
+// ShowArray(myArray);
+
+
+
+
+
+
+/*     ДВУМЕРНЫЕ МАССИВЫ - СОЗДАНИЕ, ЗАПОЛНЕННОГО СЛ. ЧИСЛАМИ     */
+/***********   Методы *************/ 
+// int[,] GetRandomArray(int m, int n, int minValue, int maxValue)
+// {
+//     int[,] result = new int[m, n];
+//     for (int i = 0; i < m; i++)
+//     {
+//         for (int j = 0; j < n; j++)
+//         {
+//             result[i, j] = new Random().Next(minValue, maxValue + 1);
+//         }
+//     }
+//     return result;
+// }
+// void PrintArray(int[,] inArray)
+// {
+//     for (int row = 0; row < inArray.GetLength(0); row++)
+//     {
+//         for (int column = 0; column < inArray.GetLength(1); column++)
+//         {
+//             Console.Write($"{inArray[row,column]} ");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+/***********   Тело кода *************/ 
+// Console.Clear();
+// Console.Write("Введите количество строк массива: ");
+// int rows=Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите количество столбцов массива: ");
+// int columns=Convert.ToInt32(Console.ReadLine());
+// int[,] array = GetRandomArray(rows, columns, 0, 10);
+// PrintArray(array);
+
+
+/*     ДВУМЕРНЫЕ МАССИВЫ - Перезаполнение массива суммой позиций по строке и столбцу    */
+// int[,] ModifyArray(int m, int n)
+// {
+//     int[,] result = new int[m, n];
+//     for (int i = 0; i < m; i++)
+//     {
+//         for (int j = 0; j < n; j++)
+//         {
+//             result[i, j] = i+j;
+//         }
+//     }
+//     return result;
+// }
+// void PrintArray(int[,] inArray)
+// {
+//     for (int row = 0; row < inArray.GetLength(0); row++)
+//     {
+//         for (int column = 0; column < inArray.GetLength(1); column++)
+//         {
+//             Console.Write($"{inArray[row,column]} ");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+/***********   Тело кода *************/ 
+// Console.Write("Введите количество строк массива: ");
+// int rows=Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите количество столбцов массива: ");
+// int columns=Convert.ToInt32(Console.ReadLine());
+// int[,] array = ModifyArray(rows, columns);
+// PrintArray(array);
+
+/******** ДВУМЕРНЫЕ МАССИВЫ **********/
+//Задача 49: Задайте двумерный массив. 
+//Найдите элементы, у которых оба индекса чётные, и замените эти элементы на их квадраты.
+int[,] GetArray(int m, int n, int minValue, int maxValue) // создает двумерный массив
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+        for (int j = 0; j < n; j++)
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+    return result;
+}
+void ModifyArrayByMultiplyItself(int [,] array2) // создает двумерный массив
+{
+    for (int i = 0; i < array2.GetLength(0); i=i+2)
+        for (int j = 0; j < array2.GetLength(1); j=j+2)
+            array2[i,j]=array2[i,j]*array2[i,j];
+}
+void PrintArray(int[,] inArray) // вывводит двумерный массив
+{
+    for (int row = 0; row < inArray.GetLength(0); row++)
+    {
+        for (int column = 0; column < inArray.GetLength(1); column++)
+            Console.Write($"{inArray[row,column]} ");
+        Console.WriteLine();
+    }
+}
+/***********   Тело кода *************/ 
+Console.Write("Введите количество строк массива: ");
+int rows=Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов массива: ");
+int columns=Convert.ToInt32(Console.ReadLine());
+int[,] array = GetArray(rows, columns, 0, 10);
+PrintArray(array);
+Console.WriteLine("------------------------------------");
+ModifyArrayByMultiplyItself(array);
+PrintArray(array);
+
+
+
+
+//Задача 51: Задайте двумерный массив. 
+//Найдите сумму элементов, находящихся на главной диагонали (с индексами (0,0); (1;1) и т.д.
+// int[,] GetArray(int m, int n, int minValue, int maxValue) // создает двумерный массив
+// {
+//     int[,] result = new int[m, n];
+//     for (int i = 0; i < m; i++)
+//         for (int j = 0; j < n; j++)
+//             result[i, j] = new Random().Next(minValue, maxValue + 1);
+//     return result; 
+// }
+// int GetSum (int [,] array) // создает двумерный массив
+// {   
+//     int sum=0;
+//     for (int i = 0; i < array.GetLength(0); i++)
+//         for (int j = 0; j < array.GetLength(1); j++)
+//             if(i==j) sum=sum+array[i,j];
+//     return sum;
+// } 
+// void PrintArray(int[,] inArray) // выводит двумерный массив
+// {
+//     for (int row = 0; row < inArray.GetLength(0); row++)
+//     {
+//         for (int column = 0; column < inArray.GetLength(1); column++)
+//             Console.Write($"{inArray[row,column]} ");
+//         Console.WriteLine();
+//     }
+// }
+/***********   Тело кода *************/
+// Console.Clear();
+// Console.Write("Введите количество строк массива: ");
+// int rows=Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите количество столбцов массива: ");
+// int columns=Convert.ToInt32(Console.ReadLine());
+// int[,] array = GetArray(rows, columns, 0, 10);
+// PrintArray(array);
+// Console.WriteLine("Сумма элементов на диагонали="+GetSum(array));
---
+++ Sem_07/home_work/Program.cs
@@ -0,0 +1,237 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// (дублирует задачу № 38)
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+    // /*****************  ОБЪЯВЛЕНИЕ МЕТОДОВ  ***********************/
+    // /* Метод для создания массива размера size из случайных чисел из диапазона [start;end] */
+    // double[,] GenerateRandomMatrix(int countRows, int countColumns, int start, int end)  
+    // {
+    //     double[,] result = new double[countRows, countColumns];  // объявляем массив размера mхn, т.е. выделение памяти для mхn переменных
+    //     for (uint i = 0; i < countRows; i++)           
+    //         for (uint j = 0; j < countColumns; j++)                         // Генерируем число из диапазона [start;end] 
+    //            result[i,j] = new Random().NextDouble()*(end-start) + start; // c масштабирванием и записываем его в [i,j]-й элемент массива                                                                                                                        
+    //     return result;
+    // }
+    // /* Метод для вывода элементов массива в консоль */
+    // void ShowMatrix(double[,] matrix)  
+    // {                             
+    //     uint rows = (uint)matrix.GetLength(0); 
+    //     uint columns =  (uint)matrix.GetLength(1); 
+    //     for (uint i = 0; i < rows; i++)
+    //     {
+    //         for (uint j = 0; j < columns; j++) 
+    //            Console.Write("{0:f2}\t", matrix[i, j]);  
+    //         Console.WriteLine();
+    //     }
+    // }
+    // /**********  ВЫЗОВ РАНЕЕ ОБЪЯВЛЕННЫХ МЕТОДОВ ~ VOID MAIN  *****************/
+    // Console.WriteLine("\n-------"); 
+    // Console.Write("Введите количество (m) строк в матрице и нажмите enter, m = "); 
+    //     int m = Convert.ToInt32(Console.ReadLine()); 
+    // Console.Write("Введите количество (m) строк в матрице и нажмите enter, m = "); 
+    //     int n = Convert.ToInt32(Console.ReadLine()); 
+    // Console.WriteLine("Введите первое число случайно генерируемого диапазона и нажмите enter:"); 
+    //     int min = Convert.ToInt32(Console.ReadLine()); 
+    // Console.WriteLine("Введите последнее число случайно генерируемого диапазона и нажмите enter:"); 
+    //     int max = Convert.ToInt32(Console.ReadLine());  
+    // double[,] randomMatrix = GenerateRandomMatrix(m, n, min, max); //перегруженный оператор присваивания с вызовом конструктора
+    // Console.WriteLine("Сгенерирована матрица: ");
+    // ShowMatrix(randomMatrix); 
+    // Console.WriteLine("-------\n"); 
+
+
+// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+    // /*****************  ОБЪЯВЛЕНИЕ МЕТОДОВ  ***********************/
+    // /* Метод для создания массива размера size из случайных чисел из диапазона [start;end] */
+    // int[,] GenerateRandomMatrix(int countRows, int countColumns, int start, int end)  
+    // {
+    //     int[,] result = new int[countRows, countColumns];  // объявляем массив размера mхn, т.е. выделение памяти для mхn переменных
+    //     for (uint i = 0; i < countRows; i++)           
+    //         for (uint j = 0; j < countColumns; j++)                         
+    //            result[i,j] = new Random().Next(start, end+1); // генерируем число из диапазона [start;end] 
+    //     return result;
+    // }
+    // /* Метод для вывода элементов массива в консоль */
+    // void ShowMatrix(int[,] matrix)  
+    // {                             
+    //     uint rows = (uint)matrix.GetLength(0); 
+    //     uint columns =  (uint)matrix.GetLength(1); 
+    //     for (uint i = 0; i < columns; i++)
+    //     {
+    //         if(i==0) Console.Write("  {0}\t", i+1);
+    //         else     Console.Write("{0}\t", i+1);
+    //     }
+    //      Console.WriteLine();
+    //     for (uint i = 0; i < columns; i++)
+    //     {
+    //         if(i==0) Console.Write("  __\t");
+    //         else     Console.Write("__\t");
+    //     }
+    //     Console.WriteLine();
+    //     for (uint i = 0; i < rows; i++)
+    //     {
+    //         Console.Write("{0}| ", i+1);
+    //         for (uint j = 0; j < columns; j++) 
+    //            Console.Write("{0}\t", matrix[i, j]);  
+    //         Console.WriteLine();
+    //     }
+    // }
+    // /* Метод для нахождения элемента по индексам (есть либо нет) */
+    // void PrintElementByPosition(int[,] matrix, int indexByRow, int indexByColumn)  
+    // {                             
+    //     uint rows = (uint)matrix.GetLength(0); 
+    //     uint columns =  (uint)matrix.GetLength(1); 
+
+    //     if (indexByRow < 0 || indexByColumn < 0)
+    //         Console.Write("Некорректные координаты позиции элемента: a({0},{1})\n", indexByRow+1, indexByColumn+1);
+    //     else if (indexByRow > rows || indexByColumn > columns)
+    //         Console.Write("элемента a({0},{1}) нет!\n", indexByRow+1, indexByColumn+1);
+    //     else
+    //         Console.Write("элемент a({0},{1}) = {2}\n", indexByRow+1, 
+    //                                                     indexByColumn+1,
+    //                                                     matrix[indexByRow,indexByColumn]);
+    // }
+    // /**********  ВЫЗОВ РАНЕЕ ОБЪЯВЛЕННЫХ МЕТОДОВ ~ VOID MAIN  *****************/
+    // /* Ввод данных */
+    // Console.WriteLine("\n-------"); 
+    // Console.Write("Введите количество (m) строк в матрице и нажмите enter, m = "); 
+    //     int m = Convert.ToInt32(Console.ReadLine()); 
+    // Console.Write("Введите количество (n) столбцов в матрице и нажмите enter, n = "); 
+    //     int n = Convert.ToInt32(Console.ReadLine()); 
+    // Console.WriteLine("Введите первое число случайно генерируемого диапазона и нажмите enter:"); 
+    //     int min = Convert.ToInt32(Console.ReadLine()); 
+    // Console.WriteLine("Введите последнее число случайно генерируемого диапазона и нажмите enter:"); 
+    //     int max = Convert.ToInt32(Console.ReadLine());  
+    // /* Создание матрицы */
+    // int[,] randomMatrix = GenerateRandomMatrix(m, n, min, max); //перегруженный оператор присваивания с вызовом конструктора
+    // Console.WriteLine("Сгенерирована матрица: ");
+    // ShowMatrix(randomMatrix); 
+    // /* Выполнение условий задачи - вывод элемента по ПОЗИЦИИ */
+    // Console.Write("\nПоиск элемента a(i,j) двумерного массива по позиции - нумерация начинается с 1"); 
+    // Console.Write("\n Введите позицию по строке и нажмите enter, i = ");
+    //     int positionRow = Convert.ToInt32(Console.ReadLine()); 
+    // Console.Write(" Введите позицию по столбцу и нажмите enter, j = ");
+    //     int positionColumn = Convert.ToInt32(Console.ReadLine()); 
+    // Console.Write("Результат: ");
+    // PrintElementByPosition(randomMatrix, positionRow-1, positionColumn-1);
+    // Console.WriteLine("-------\n"); 
+
+
+// Задача 52. Задайте двумерный массив из целых чисел. 
+// Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+    /*****************  ОБЪЯВЛЕНИЕ МЕТОДОВ  ***********************/
+    /* Важно: здесь интерпритация работы с классом Matrix, п.э. векторов нет
+    /* Метод для создания массива размера size из случайных чисел из диапазона [start;end] */
+    int[,] GenerateRandomMatrix(int countRows, int countColumns, int start, int end)  
+    {
+        int[,] result = new int[countRows, countColumns];  // объявляем массив размера mхn, т.е. выделение памяти для mхn переменных
+        for (uint i = 0; i < countRows; i++)           
+            for (uint j = 0; j < countColumns; j++)                         
+               result[i,j] = new Random().Next(start, end+1); // генерируем число из диапазона [start;end] 
+        return result;
+    }
+    /* Метод для вывода элементов массива в консоль */
+    void ShowMatrix(int[,] matrix)  
+    {                             
+        uint rows = (uint)matrix.GetLength(0); 
+        uint columns =  (uint)matrix.GetLength(1); 
+        for (uint i = 0; i < columns; i++)
+        {
+            if(i==0) Console.Write("  {0}\t", i+1);
+            else     Console.Write("{0}\t", i+1);
+        }
+         Console.WriteLine();
+        for (uint i = 0; i < columns; i++)
+        {
+            if(i==0) Console.Write("  __\t");
+            else     Console.Write("__\t");
+        }
+        Console.WriteLine();
+        for (uint i = 0; i < rows; i++)
+        {
+            Console.Write("{0}| ", i+1);
+            for (uint j = 0; j < columns; j++) 
+               Console.Write("{0}\t", matrix[i, j]);  
+            Console.WriteLine();
+        }
+    }
+    /* Перегрузка метода вывода матрицы для типа double */
+    void ShowMatrixDoubleType(double[,] matrix)  
+    {                             
+        uint rows = (uint)matrix.GetLength(0); 
+        uint columns =  (uint)matrix.GetLength(1); 
+        for (uint i = 0; i < columns; i++)
+        {
+            if(i==0) Console.Write("  {0}\t", i+1);
+            else     Console.Write("{0}\t", i+1);
+        }
+         Console.WriteLine();
+        for (uint i = 0; i < columns; i++)
+        {
+            if(i==0) Console.Write("  __\t");
+            else     Console.Write("__\t");
+        }
+        Console.WriteLine();
+        for (uint i = 0; i < rows; i++)
+        {
+            Console.Write("{0}| ", i+1);
+            for (uint j = 0; j < columns; j++) 
+               Console.Write("{0:f1}\t", matrix[i, j]);  
+            Console.WriteLine();
+        }
+    }
+    /* Метод для нахождения стредних значений по столбцам */
+    double[,] AverrangeByMatrixColumn(int[,] matrix)  
+    {                             
+        uint countRows = (uint)matrix.GetLength(0); 
+        uint countColumns =  (uint)matrix.GetLength(1); 
+        double[,] result = new double[1,countColumns];  // объявляем массив размера columns, для хранения среднего по столбцу
+        for (uint j = 0; j < countColumns; j++)
+        {
+            for (uint i = 0; i < countRows; i++) 
+                if (i == 0) // чтоб не обнулять массив и не вводить врем. переменную
+                    result[0,j] = matrix[i, j]; 
+                else
+                    result[0,j] += matrix[i, j];
+            result[0,j] = result[0,j]/(double)countRows; // собрали все по столбцу и разделили на количество 
+        }                                                // типа double, иначе округлит до int
+        return result;
+    }
+    /**********  ВЫЗОВ РАНЕЕ ОБЪЯВЛЕННЫХ МЕТОДОВ ~ VOID MAIN  *****************/
+    /* Ввод данных */
+    Console.WriteLine("\n-------"); 
+    Console.Write("Введите количество (m) строк в матрице и нажмите enter, m = "); 
+        int m = Convert.ToInt32(Console.ReadLine()); 
+    Console.Write("Введите количество (n) столбцов в матрице и нажмите enter, n = "); 
+        int n = Convert.ToInt32(Console.ReadLine()); 
+    Console.WriteLine("Введите первое число случайно генерируемого диапазона и нажмите enter:"); 
+        int min = Convert.ToInt32(Console.ReadLine()); 
+    Console.WriteLine("Введите последнее число случайно генерируемого диапазона и нажмите enter:"); 
+        int max = Convert.ToInt32(Console.ReadLine());  
+    /* Создание матрицы */
+    int[,] randomMatrix = GenerateRandomMatrix(m, n, min, max); //перегруженный оператор присваивания с вызовом конструктора
+    Console.WriteLine("\nСгенерирована матрица: ");
+    ShowMatrix(randomMatrix); 
+    /* Выполнение условий задачи - сренднее значение матрицы по столбцам */
+    Console.WriteLine("\n****** Результат ******");
+    Console.WriteLine("Среднее значение матрицы по столбцам:"); 
+    double[,] averrangeMatrix = AverrangeByMatrixColumn(randomMatrix); 
+    ShowMatrixDoubleType(averrangeMatrix); 
+    Console.WriteLine("-------\n"); 
+
+
+
+
